e6cc3dbae60ff3bcdd582149bfc869e35878f3aa
Duncan <mail.d.luk@gmail.com>
2021-05-20 23:04:07 +0200

    Create output directory if it doesn't exist yet

--- src/reporting/writeResults.ts
+++ src/reporting/writeResults.ts
@@ -4,8 +4,14 @@ export default async function writeResultsToCsv(values: {
   spentEur: number;
   balanceEur: number;
 }): Promise<void> {
+  const directory = "./output";
+
+  if (!(await directoryExists(directory))) {
+    await fs.promises.mkdir(directory);
+  }
+
   await fs.promises.appendFile(
-    "./output/report.csv",
+    directory + "/report.csv",
     new Date().getTime() +
       "," +
       values.spentEur +
@@ -13,4 +19,13 @@ export default async function writeResultsToCsv(values: {
       values.balanceEur +
       "\n"
   );
+}
+
+async function directoryExists(path: string): Promise<boolean> {
+  try {
+    await fs.promises.access(path, fs.constants.F_OK);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
