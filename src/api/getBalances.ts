1e84f413b4e97b39192c8e4184e5e6c966ed8101
Dennis Decoene <DDecoene@users.noreply.github.com>
2022-02-12 15:10:44 +0100

    added types in hopes of more readability

--- src/api/getBalances.ts
+++ src/api/getBalances.ts
@@ -3,32 +3,40 @@ import config from "../config";
 import callApi from "./helpers/callApi";
 import createSignature from "./helpers/createSignature";
 
-export default async function getBalances(
-  options: {
-    symbol?: string;
-  } = {}
-): Promise<
-  Array<{
-    symbol: string;
-    available: number;
-    inOrder: number;
-  }>
-> {
-  const response = await callApi<
-    Array<{
-      symbol: string;
-      available: string;
-      inOrder: string;
-    }>
-  >({
+interface BalanceResult {
+  symbol: string;
+  available: string;
+  inOrder: string;
+}
+
+interface Balance {
+  symbol: string;
+  available: number;
+  inOrder: number;
+}
+
+type BalanceResultList = Array<BalanceResult>
+type BalanceList = Array<Balance>
+
+export default async function getBalances(options: { symbol?: string } = {}): Promise<BalanceList> {
+  const response = await callApi<BalanceResultList>({
     method: "GET",
     path: "/balance",
     query: options,
   });
 
-  return response.map((currency) => ({
-    symbol: currency.symbol,
-    available: parseFloat(currency.available),
-    inOrder: parseFloat(currency.inOrder),
-  }));
+  let r: BalanceList = []
+
+  response.forEach(
+    (b: BalanceResult) => {
+      r.push(
+        {
+          symbol: b.symbol,
+          available: parseFloat(b.available),
+          inOrder: parseFloat(b.inOrder)
+        })
+    }
+  )
+
+  return r
 }
